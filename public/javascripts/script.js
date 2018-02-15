c6ed08df5e1a2cc79bf8fe41cbbde8c3ef140dd3
Daniel <ddegrassi17@gmail.com>
2018-02-15 15:54:03 +0000

    portfolio page with mixitup, rough cut but working

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,16 +1,28 @@
-// First page set height to browser window
-$('.firstpage').ready(function() {
-  function setHeight() {
-    windowHeight = $(window).innerHeight();
-    $('.firstpage').css('min-height', windowHeight);
-  };
-  setHeight();
-  
-  $(window).resize(function() {
+// Finds height of browser window
+var pageHeights = function(page) {
+  $(page).ready(function() {
+    function setHeight() {
+      windowHeight = $(window).innerHeight();
+      $(page).css('min-height', windowHeight);
+    };
     setHeight();
-  });
-});
+    $(window).resize(function() {
+      setHeight();
+    });
 
+    paddingTop = String(windowHeight * .3 + "px");
+    $('.firstpagecontent').css('padding-top', paddingTop);
+  });
+};
+
+// Sets each page to proper height
+pageHeights('.firstpage');
+pageHeights('#about');
+pageHeights('#portfolio')
+pageHeights('#contact');
+
+// instatianes .portfolio-container mixer
+var mixer = mixitup('.portfolio-container');
 
 // Color changing background: to line 76
 var colors = new Array(
@@ -80,24 +92,39 @@ var sticky = new Waypoint.Sticky({
   element: $('.navbar')[0]
 });
 
-// Scrolls to 2nd page when clicked
-$('#scrollto2ndpage').click(function(event) {
-  event.preventDefault();
-  $('html, body').animate({
-    scrollTop: $('#secondpage').offset().top}, 500);
-});
+// Function to Scroll to a target when something is clicked
+var scrollToPage = function(clicked, target) {
+  $(clicked).click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+    scrollTop: $(target).offset().top}, 500);
+  });
+};
+
+// Scroll to 'About' page when button on first page is clicked
+scrollToPage('#scrollto2ndpage', '#about');
+
+// Scrolls to each page when you click on navbar
+scrollToPage('#homebutton', '.firstpage');
+scrollToPage('#aboutbutton', '#about');
+scrollToPage('#portfoliobutton', '#portfolio');
+scrollToPage('#contactbutton', '#contact');
+
 
 // Sends arrow off the page when hovered over
 $('#scrollto2ndpage').hover(function() {
+  // invalidates for small screens
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
     return;  
   } else {
+    // finds exactly where the arrow will be located as it moves down (and out of the 'scrollto2ndpage' div it starts in)
     var elmnt = document.getElementById("scrollto2ndpage");
     var arrowlocation = String(elmnt.offsetWidth / 2 - 12) + "px";
-    console.log(arrowlocation);
-    $('#moveable-fav').css("position", "fixed").css("margin-left", arrowlocation).animate({marginTop: "1000px"});
+    // moves the arrow down
+    $('#moveable-fav').css("position", "fixed").css("margin-left", arrowlocation).animate({marginTop: "1000px"}, 700);
+    // sets the scrollto2ndpage div to accomodate for the missing favicon
     $('#scrollto2ndpage').css("padding-right", "37px").css("padding-left", "4.5px");
-
-  }
-  
+  } 
 });
+
+
