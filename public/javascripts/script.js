024d415979422f7f5afd806cd4bf65a750e326a7
Daniel <ddegrassi17@gmail.com>
2018-03-12 18:39:19 +0000

    content written, cleaned up some small issues and css

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -100,6 +100,8 @@ scrollToPage('#homebutton', '.firstpage');
 scrollToPage('#aboutbutton', '#about-waypoint');
 scrollToPage('#portfoliobutton', '#portfolio-waypoint');
 scrollToPage('#contactbutton', '#contact-waypoint');
+// Scrolls to contact page from about blurb
+scrollToPage('#q-or-op', '#contact-waypoint');
 // Scrolls back to top from footer
 scrollToPage('#back-to-top-div', '.firstpage');
 
@@ -128,53 +130,29 @@ $("#contact-form").submit(function(){
 
     // Custom thank you for sending an email
     var cc = $('#contact-container');
+    var ty = $('#thank-you');
       cc.animate({
       width: '0px',
       height: '0px'
       }, 500
     );
-    var ty = $('#thank-you');
+
     setTimeout(function(){
       cc.css('display', 'none')
-      }, 499
+      }, 480
     );
     setTimeout(function(){
       ty.css('display', 'initial')
-      }, 501
+      }, 520
 
     );
     setTimeout(function(){
       ty.animate({color: 'white', borderColor: 'white'}, 1000);
       }, 550
     );
-
     return false;
 });
 
-// MODAL TIME
-
-// var modal = document.getElementById('myModal')
-//   var trigger = document.getElementById("myportfolio");
-//   var closeButton = document.querySelector(".close");
-// console.log(trigger)
-//   function toggleModal() {
-//       modal.classList.toggle("show-modal");
-//   }
-
-//   function windowOnClick(event) {
-//       if (event.target === modal) {
-//           toggleModal();
-//       }
-//   }
-// $('#gallery .button').click(function() {
-//   console.log('sup dawg');
-//   modal.classList.toggle("show-modal");
-// });
-
-
-// closes modals
-// closeButton.addEventListener("click", toggleModal);
-// window.addEventListener("click", windowOnClick);
 
 // MODAL
 $(document).ready(function() {
@@ -182,36 +160,36 @@ $(document).ready(function() {
     chateau: {
       title: 'Château de Missery',
       tag: 'CASTLE IN FRANCE',
-      detail: 'Talk all about the castle and the website itself',
+      detail: 'Built a responsive, budget friendly, search engine optimized, and beautiful web application using Ruby, HTML, CSS, with a touch of Javascript. I integrated a free CMS to allow the client to update and create new content and pages on their site',
       link: 'http://chateaudemissery.herokuapp.com/',
-      img: '#'
+      img: '/images/chateau4.png'
     },
     myportfolio: {
-      title: 'dgdegrassiportfolio',
-      tag: 'all about me',
-      detail: 'talk about this portfolio and why its featured here',
-      img: '/images/modaltest.png'
+      title: 'My Portfolio',
+      tag: 'Deep dive in to Javascript',
+      detail: "When I started building this website, I only had a small amount of experience with Javascript, and none with Nodejs.  I've since became very comfortable with and gained an appreciation for both. I wrote the code above after being unable to find a form submitted response clean enough for what I wanted.  If you want to see what it does, say hi!  This just a few of the many lines of code that I'm proud of in this project.",
+      img: '/images/portfolio1.png'
     }, 
     delphi: {
       title: 'Delphi',
       tag: 'Student Project',
       detail: 'A Yelp clone that integrates with the Google Maps API and includes features like user comments, star ratings, image uploading, and user authentication.',
       link: 'https://delphi-daniel-degrassi.herokuapp.com/',
-      img: '#'
+      img: '/images/delphi2.png'
     },
     flixster: {
       title: 'Flixster',
-      tag: 'STUDENT PROJECT',
+      tag: 'Student Project',
       detail: 'A two-sided, video-streaming marketplace platform that features credit card payment capabilities, user role management, complex user interfaces, and advanced database relationships.',
       link: 'https://flixter-dan-degrassi.herokuapp.com/',
-      img: '#'
+      img: '/images/flixster1.png'
     },
     todo: {
       title: 'Todoster',
-      tag: 'STUDENT PROJECT',
-      detail: 'This single-page to-do application features a fluid user interface that– by using JavaScript– allows users to rapidly add dynamic content.',
+      tag: 'Student Project',
+      detail: 'This single-page to-do application features a fluid user interface that allows users to rapidly add dynamic content.',
       link: 'https://todoster-dan-degrassi.herokuapp.com/',
-      img: '#'
+      img: '/images/todo1.png'
     }
   };
 
